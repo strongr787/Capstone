cbf01bc7b5cd286a53650845973722150edbb778
Ploiu <curtislanglitz+github@gmail.com>
2020-03-07 15:09:07 -0500

    Main screen, no functionality for now

--- Capstone/MainPage.xaml.cs
+++ Capstone/MainPage.xaml.cs
@@ -22,9 +22,55 @@ namespace Capstone
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private bool IsMenuExpanded = false;
         public MainPage()
         {
             this.InitializeComponent();
+            // hide the main menu
+            this.MenuColumn.Width = new GridLength(0);
+        }
+
+        private void MenuButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            if(!this.IsMenuExpanded)
+            {
+                // show the menu column
+                this.MenuColumn.Width = new GridLength(320);
+            } else
+            {
+                this.MenuColumn.Width = new GridLength(0);
+            }
+            this.IsMenuExpanded = !this.IsMenuExpanded;
+        }
+
+        private void SettingsButton_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO navigate to settings screen (this.Frame.Navigate(typeof(screenName)))
+        }
+
+        private void RemindersButton_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO navigate to settings screen (this.Frame.Navigate(typeof(screenName)))
+        }
+
+        private void AlarmsButton_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO navigate to settings screen (this.Frame.Navigate(typeof(screenName)))
+        }
+
+        private void VoiceNotesButton_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO navigate to settings screen (this.Frame.Navigate(typeof(screenName)))
+        }
+
+        private void DataPrivacyButton_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO navigate to settings screen (this.Frame.Navigate(typeof(screenName)))
+        }
+
+        private void LibrariesButton_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO navigate to settings screen (this.Frame.Navigate(typeof(screenName)))
         }
     }
 }
