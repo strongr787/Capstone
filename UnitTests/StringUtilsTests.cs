ebc6085952ce009059f85cf4db0cef243593d695
Ploiu <curtislanglitz+github@gmail.com>
2020-03-12 11:27:25 -0400

    string utils tests

---
+++ UnitTests/StringUtilsTests.cs
@@ -0,0 +1,38 @@
+﻿using Capstone.Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class StringUtilsTests
+    {
+        [TestMethod]
+        public void TestIsBlankReturnsTrueForNull()
+        {
+            Assert.IsTrue(StringUtils.IsBlank(null));
+        }
+
+        [TestMethod]
+        public void TestIsBlankReturnsTrueForEmpty()
+        {
+            Assert.IsTrue(StringUtils.IsBlank(""));
+        }
+
+        [TestMethod]
+        public void TestIsBlankReturnsTrueForOnlyWhitespace()
+        {
+            Assert.IsTrue(StringUtils.IsBlank("   \t\n"));
+        }
+
+        [TestMethod]
+        public void TestIsBlankReturnsFalseForNonBlankString()
+        {
+            Assert.IsFalse(StringUtils.IsBlank("null hi   "));
+        }
+    }
+}
