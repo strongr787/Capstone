b5405d0e9d4417219831b2310787bb9678d5cceb
CurtisJ123 <54820658+CurtisJ123@users.noreply.github.com>
2020-03-29 23:54:12 -0400

    Update App.xaml.cs

--- Capstone/App.xaml.cs
+++ Capstone/App.xaml.cs
@@ -1,210 +1,102 @@
-using Microsoft.Data.Sqlite;
+using Capstone.Common;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
 
-namespace Capstone.Common
+namespace Capstone
 {
-    class StoredProcedures
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    sealed partial class App : Application
     {
-        public static async Task CreateDatabase()
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
         {
-            string targetDbPath = Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path, "Database\\BobDB.db");
-            if (!File.Exists(targetDbPath))
+            this.InitializeComponent();
+            this.Suspending += OnSuspending;
+            StoredProcedures.CreateDatabase();
+        }
+
+        /// <summary>
+        /// Invoked when the application is launched normally by the end user.  Other entry points
+        /// will be used such as when the application is launched to open a specific file.
+        /// </summary>
+        /// <param name="e">Details about the launch request and process.</param>
+        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        {
+            Frame rootFrame = Window.Current.Content as Frame;
+
+            // Do not repeat app initialization when the Window already has content,
+            // just ensure that the window is active
+            if (rootFrame == null)
             {
-                var installedLocation = Windows.ApplicationModel.Package.Current.InstalledLocation;
-                using (var input = await installedLocation.OpenStreamForReadAsync("Assets\\BobDB.db"))
+                // Create a Frame to act as the navigation context and navigate to the first page
+                rootFrame = new Frame();
+
+                rootFrame.NavigationFailed += OnNavigationFailed;
+
+                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
                 {
-                    using (var output = await Windows.Storage.ApplicationData.Current.LocalFolder.OpenStreamForWriteAsync("Database\\BobDB.db", Windows.Storage.CreationCollisionOption.FailIfExists))
-                    {
-                        await input.CopyToAsync(output);
-                    }
+                    //TODO: Load state from previously suspended application
                 }
-            }
-        }
-        public static SqliteConnection OpenDatabase()
-        {
-            string targetDbPath = Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path, "Database\\BobDB.db");
-            SqliteConnection conn = new SqliteConnection(@"Data Source = " + targetDbPath);
-            return conn;
-        }
 
-        public static void CreateReminder(string Title, DateTime Time, DateTime Date)
-        {
+                // Place the frame in the current Window
+                Window.Current.Content = rootFrame;
+            }
 
-            String strTime = Time.ToString("yyyy-MM-dd HH:mm");
-            String strDate = Date.ToString("yyyy-MM-dd HH:mm");
-            SqliteConnection conn = OpenDatabase();
-            conn.Open();
-            SqliteCommand command = conn.CreateCommand();
-            command.CommandText = $"INSERT INTO TReminders(reminderTitle, reminderTime, isDeleted) Values('{Title}','{strTime}', 0);";
-            command.ExecuteNonQuery();
-            command.CommandText = $"INSERT INTO TReminderDates(reminderID, reminderDate) Select MAX(reminderID), '{strDate}' FROM TReminders;";
-            command.ExecuteNonQuery();
-            conn.Close();
-        }
-        public static void UpdateReminder(int ID, string Title, DateTime Time, DateTime Date)
-        {
-            int intID = ID;
-            String strTime = Time.ToString("yyyy-MM-dd HH:mm");
-            String strDate = Date.ToString("yyyy-MM-dd HH:mm");
-            SqliteConnection conn = OpenDatabase();
-            conn.Open();
-            SqliteCommand command = conn.CreateCommand();
-            command.CommandText = $"Update TReminders Set reminderTime = '{Time}', reminderTitle = '{Title}' Where reminderID = {intID};";
-            command.ExecuteNonQuery();
-            command.CommandText = $"Update TReminderDates Set reminderDate = '{Date}' Where reminderID = {intID};";
-            command.ExecuteNonQuery();
-            conn.Close();
-        }
-        public static void DeleteReminder(int ID)
-        {
-            int intID = ID;
-            SqliteConnection conn = OpenDatabase();
-            conn.Open();
-            SqliteCommand command = conn.CreateCommand();
-            command.CommandText = $"Update TReminders Set isDeleted = 1 Where reminderID = {intID};";
-            command.ExecuteNonQuery();
-            conn.Close();
-        }
-        public static string QueryReminder(int ID = -1)
-        {
-            String strData = "";
-            string intID;
-            if (ID == -1)
+            if (e.PrelaunchActivated == false)
             {
-                intID = "null";
-            }
-            else
-            {
-                intID = ID.ToString();
-            }
-            
-            SqliteConnection conn = OpenDatabase();
-            conn.Open();
-            SqliteCommand command = conn.CreateCommand();
-            command.CommandText = $"Select TReminders.reminderID, TReminders.reminderTitle, TReminders.reminderTime, TReminderDates.reminderDate, TReminders.isDeleted From TReminders, TReminderDates Where TReminders.reminderID = TReminderDates.reminderID and TReminders.reminderID = COALESCE({intID}, TReminders.reminderID); ";
-            using (SqliteDataReader reader = command.ExecuteReader())
-            {
-                while (reader.Read())
+                if (rootFrame.Content == null)
                 {
-                    strData += reader.GetString(1);
+                    // When the navigation stack isn't restored navigate to the first page,
+                    // configuring the new page by passing required information as a navigation
+                    // parameter
+                    rootFrame.Navigate(typeof(FirstTimeSetup), e.Arguments);
                 }
+                // Ensure the current window is active
+                Window.Current.Activate();
             }
-            conn.Close();
-            return strData;
         }
-        public static void CreateAlarm(string Title, DateTime Time, DateTime Date)
-        {
 
-            String strTime = Time.ToString("yyyy-MM-dd HH:mm");
-            String strDate = Date.ToString("yyyy-MM-dd HH:mm");
-            SqliteConnection conn = OpenDatabase();
-            conn.Open();
-            SqliteCommand command = conn.CreateCommand();
-            command.CommandText = $"INSERT INTO TAlarms(AlarmTitle, AlarmTime, isDeleted) Values('{Title}','{strTime}', 0);";
-            command.ExecuteNonQuery();
-            command.CommandText = $"INSERT INTO TAlarmDates(AlarmID, AlarmDate) Select MAX(AlarmID), '{strDate}' FROM TAlarms;";
-            command.ExecuteNonQuery();
-            conn.Close();
-        }
-        public static void UpdateAlarm(int ID, string Title, DateTime Time, DateTime Date)
+        /// <summary>
+        /// Invoked when Navigation to a certain page fails
+        /// </summary>
+        /// <param name="sender">The Frame which failed navigation</param>
+        /// <param name="e">Details about the navigation failure</param>
+        void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
         {
-            int intID = ID;
-            String strTime = Time.ToString("yyyy-MM-dd HH:mm");
-            String strDate = Date.ToString("yyyy-MM-dd HH:mm");
-            SqliteConnection conn = OpenDatabase();
-            conn.Open();
-            SqliteCommand command = conn.CreateCommand();
-            command.CommandText = $"Update TAlarms Set AlarmTime = '{Time}', AlarmTitle = '{Title}' Where AlarmID = {intID};";
-            command.ExecuteNonQuery();
-            command.CommandText = $"Update TAlarmDates Set AlarmDate = '{Date}' Where AlarmID = {intID};";
-            command.ExecuteNonQuery();
-            conn.Close();
+            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
         }
-        public static void DeleteAlarm(int ID)
-        {
-            int intID = ID;
-            SqliteConnection conn = OpenDatabase();
-            conn.Open();
-            SqliteCommand command = conn.CreateCommand();
-            command.CommandText = $"Update TAlarms Set isDeleted = 1 Where AlarmID = {intID};";
-            command.ExecuteNonQuery();
-            conn.Close();
-        }
-        public static string QueryAlarm(int ID = -1)
-        {
-            String strData = "";
-            string intID;
-            if (ID == -1)
-            {
-                intID = "null";
-            }
-            else
-            {
-                intID = ID.ToString();
-            }
 
-            SqliteConnection conn = OpenDatabase();
-            conn.Open();
-            SqliteCommand command = conn.CreateCommand();
-            command.CommandText = $"Select TAlarms.AlarmID, TAlarms.AlarmTitle, TAlarms.AlarmTime, TAlarmDates.AlarmDate, TAlarms.isDeleted From TAlarms, TAlarmDates Where TAlarms.AlarmID = TAlarmDates.AlarmID and TAlarms.AlarmID = COALESCE({intID}, TAlarms.AlarmID); ";
-            using (SqliteDataReader reader = command.ExecuteReader())
-            {
-                while (reader.Read())
-                {
-                    strData += reader.GetString(1);
-                }
-            }
-            conn.Close();
-            return strData;
-        }
-        public static void CreateVoiceNote(string FileName, string DsiplayName, int RecordingDuration, string FilePath, DateTime RecordDate, DateTime RecordTime)
+        /// <summary>
+        /// Invoked when application execution is being suspended.  Application state is saved
+        /// without knowing whether the application will be terminated or resumed with the contents
+        /// of memory still intact.
+        /// </summary>
+        /// <param name="sender">The source of the suspend request.</param>
+        /// <param name="e">Details about the suspend request.</param>
+        private void OnSuspending(object sender, SuspendingEventArgs e)
         {
-
-            String strRecordTime = RecordTime.ToString("yyyy-MM-dd HH:mm");
-            String strRecordDate = RecordDate.ToString("yyyy-MM-dd HH:mm");
-            SqliteConnection conn = OpenDatabase();
-            conn.Open();
-            SqliteCommand command = conn.CreateCommand();
-            command.CommandText = $"INSERT INTO TVoiceMemos(fileName,displayName,recordingDuration,filePath,recordDate,recordTime) Values('{FileName}', '{DsiplayName}', '{RecordingDuration}', '{FilePath}', '{strRecordDate}', '{strRecordTime}'); ";
-            command.ExecuteNonQuery();
-            conn.Close();
-        }
-        public static void DeleteVoiceNote(int ID)
-        {
-            int intID = ID;
-            SqliteConnection conn = OpenDatabase();
-            conn.Open();
-            SqliteCommand command = conn.CreateCommand();
-            command.CommandText = $"Delete From TVoiceMemos Where TVoiceMemos.voiceMemoID = {intID};";
-            command.ExecuteNonQuery();
-            conn.Close();
-        }
-        public static void UpdateVoiceNote(int ID, string Title)
-        {
-            int intID = ID;
-            SqliteConnection conn = OpenDatabase();
-            conn.Open();
-            SqliteCommand command = conn.CreateCommand();
-            command.CommandText = $"Update TVoiceMemos Set displayName = '{Title}' Where voiceMemoID = {intID};";
-            command.ExecuteNonQuery();
-            conn.Close();
-        }
-        public static void UpdateSettings(int ID, bool IsSelected)
-        {
-            int intID = ID;
-            bool boolIsSelected = IsSelected;
-            int intIsSelected = boolIsSelected ? 1 : 0;
-            SqliteConnection conn = OpenDatabase();
-            conn.Open();
-            SqliteCommand command = conn.CreateCommand();
-            command.CommandText = $"Update TSettingOptions Set isSelected = {intIsSelected} Where settingOptionID = {intID};";
-            command.ExecuteNonQuery();
-            conn.Close();
+            var deferral = e.SuspendingOperation.GetDeferral();
+            //TODO: Save application state and stop any background activity
+            deferral.Complete();
         }
     }
 }
