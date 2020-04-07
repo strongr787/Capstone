5fc686ef6285842d1998104ab36886efea23a739
ploiu <curtislanglitz+github@gmail.com>
2020-04-07 16:25:24 -0400

    new permissions to the manifest and remove debug code

--- Capstone/MainPage.xaml.cs
+++ Capstone/MainPage.xaml.cs
@@ -25,12 +25,6 @@ namespace Capstone
             // hide the main menu
             this.MenuColumn.Width = new GridLength(0);
             ActionRouter.SetUp();
-
-            // DEBUG -- uncomment this code if you want to test out the weather service.
-            //string command = "what's the weather?";
-            //Func<string, Actions.Action> foundAction = ActionRouter.GetFunctionFromCommandString(command);
-            //foundAction(command).PerformAction(this.media);
-			// END DEBUG
 			
             // prevent the application from closing when the user hits the x button. This will alarms and notifications to still trigger
             SystemNavigationManagerPreview.GetForCurrentView().CloseRequested += CloseHandle;
